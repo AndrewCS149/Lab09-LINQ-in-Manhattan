352f9b68095e1ee9f2331b8728a2abf97af1a0cd
Andrew Smith <andrewcs149@gmail.com>
2020-07-16 18:24:01 -0700

    Create Feature, RootObject, Properties and Geometry classes.

---
+++ LINQInManhattan/LINQInManhattan/Classes/Geometry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQInManhattan.Classes
+{
+    public class Geometry
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+    }
+}
---
+++ LINQInManhattan/LINQInManhattan/Classes/RootObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQInManhattan.Classes
+{
+    public class Rootobject
+    {
+        public string type { get; set; }
+        public Feature[] features { get; set; }
+    }
+}
